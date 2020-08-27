2e20c2c98668be71a6fa26b6db3c8dab68acd624
wedunsto <wedunsto@aggies.ncat.edu>
2020-08-27 13:02:23 -0400

    Extended Java inheritance example

---
+++ Inheritance/Java/Civilization/src/Class/City.java
@@ -0,0 +1,70 @@
+package Class;
+
+import java.util.ArrayList;
+
+/*
+ *Objectives:
+ *	Create final derived class 'City'
+ *	Detail a city's name, zip code range, and citizens
+ */
+public class City extends State{
+	private String cityName;//Contains the city's name
+	private int zipCodes[];//Contains the range of zip codes in the city
+	private ArrayList citizens;//Contains the names of people living in the city
+
+	public City(){//Default constructor
+		this.cityName="";
+		this.zipCodes=new int[3];
+		this.citizens=new ArrayList<String>();
+	}
+
+	public City(String countryName, String countryCurrency, double countryPopulation,
+							String stateName,String stateFood,double statePopulation,
+							String cityName, int zipCode1, int zipCode2, int zipCode3){//Overloaded constructor
+		super(countryName,countryCurrency,countryPopulation,stateName,stateFood,statePopulation);
+		this.cityName=cityName;
+		this.zipCodes=new int[3];
+		this.zipCodes[0]=zipCode1;
+		this.zipCodes[1]=zipCode2;
+		this.zipCodes[2]=zipCode3;
+		this.citizens = new ArrayList<String>();
+	}
+
+	//Proper getters and setters
+	public void setCityName(String cityName){
+		this.cityName = cityName;
+	}
+
+	public String getCityName(){
+		return this.cityName;
+	}
+	public void setZipCode(int index, int zipCode){
+		this.zipCodes[index] = zipCode;
+	}
+
+	public int getZipCode(int index){
+		return this.zipCodes[index];
+	}
+
+	public int getZipCodesSize(){
+		return this.zipCodes.length;
+	}
+
+	public void addCitizen(String citizen){
+		this.citizens.add(citizen);
+	}
+
+	public String removeCitizen(int index){
+		String removedCitizen = citizens.get(index).toString();
+		citizens.remove(index);
+		return removedCitizen;
+	}
+
+	public String getCitizen(int index){
+		return citizens.get(index).toString();
+	}
+
+	public int getCitizenSize(){
+		return citizens.size();
+	}
+}
--- Inheritance/Java/Civilization/src/Class/Main.java
+++ Inheritance/Java/Civilization/src/Class/Main.java
@@ -3,10 +3,22 @@ package Class;
 public class Main {
 	public static Country USA = new Country("United States","USD",4.6);
 	public static State WA = new State("United States", "USD", 4.6, "Washington", "Coffee", 1.5);
-
+	public static City SJ = new City("United States","USD",4.6,
+																				"California","Avocado Toast",7.8,
+																				"SanJose",11111,11211,22322);
+	public static City test = new City();
 	public static void main(String[] args){
 		System.out.println("The country is: "+USA.getCountryName()+" the currency is: "+USA.getCountryCurrency()+" the population is: "+Double.toString(USA.getCountryPopulation())+" billion");
 		System.out.println("In the country: "+WA.getCountryName()+", the state is: "+WA.getStateName()+" the state is famous for: "+WA.getStateFood()+" the population is: "+
 			Double.toString(WA.getStatePopulation())+" million");
+		SJ.addCitizen("William Dunston");
+		SJ.addCitizen("Shiroe Kagi");
+		System.out.println("In the state: "+SJ.getStateName()+" the city is: "+SJ.getCityName());
+		for(int i=0;i<SJ.getZipCodesSize();i++){
+			System.out.println("The zip code is: "+Integer.toString(SJ.getZipCode(i)));
+		}
+		for(int j=0;j<SJ.getCitizenSize();j++){
+			System.out.println("The citizens include: "+SJ.getCitizen(j));
+		}
 	}
 }
